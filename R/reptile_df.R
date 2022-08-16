a6dc8a4e1a17edcbdcb6243b201c597024232d6f
gabrielhoc <gabrielhoc@gmail.com>
2022-08-16 16:28:53 +0300

    updated data documentation

--- R/reptile_df.R
+++ R/reptile_df.R
@@ -7,10 +7,18 @@
 #'   \item{sp}{character. species name}
 #'   \item{iucn_cat}{character. IUCN Red List threat category}
 #'   \item{mass}{numeric. Maximum body mass, in grams}
+#'   \item{brood_size}{numeric. Average brood size}
 #'   \item{insular}{binary. If insular endemic, value 1, if not, value 0}
 #'   \item{range_area}{numeric. Area of distribution range, in km2}
 #'   \item{human_footprint}{numeric. Average human footprint (ref) density across species distribution}
-#'   \item{pop_dens}{numeric. Average human population density across species distribution}
+#'   \item{clim_2050_245}{numeric. Uninhabitable area due to climate change in 2050, under ssp 245}
+#'   \item{clim_2100_245}{numeric. Uninhabitable area due to climate change in 2100, under ssp 245}
+#'   \item{clim_2050_585}{numeric. Uninhabitable area due to climate change in 2050, under ssp 585}
+#'   \item{clim_2100_585}{numeric. Uninhabitable area due to climate change in 2100, under ssp 585}
+#'   \item{luh_2050_245}{numeric. Uninhabitable area due to land use change in 2050, under ssp 245}
+#'   \item{luh_2100_245}{numeric. Uninhabitable area due to land use change in 2100, under ssp 245}
+#'   \item{luh_2050_585}{numeric. Uninhabitable area due to land use change in 2050, under ssp 585}
+#'   \item{luh_2100_585}{numeric. Uninhabitable area due to land use change in 2100, under ssp 585}
 #'   ...
 #' }
 "reptile_df"
